6f4732d6b34adf82ee8eda0e0eec92fa404c4d04
Jean-Gabriel Gaudreault <jean-gabriel.gaudreault@hotmail.com>
2017-11-22 12:41:36 -0500

    Some changes for FridgeActivity UI

--- FamilyTaskManager/app/src/main/java/familytaskmanager/microso/com/familytaskmanager/FridgeActivity.java
+++ FamilyTaskManager/app/src/main/java/familytaskmanager/microso/com/familytaskmanager/FridgeActivity.java
@@ -1,13 +1,42 @@
 package familytaskmanager.microso.com.familytaskmanager;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class FridgeActivity extends AppCompatActivity {
+public class FridgeActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fridge);
+
+        Button addItem = (Button) findViewById(R.id.addItemButton);
+        addItem.setOnClickListener(FridgeActivity.this);
+    }
+
+    @Override
+    public void onClick(View v){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        final EditText et = new EditText(this);
+
+        // set prompts.xml to alertdialog builder
+        alertDialogBuilder.setView(et);
+
+        // set dialog message
+        alertDialogBuilder.setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+            }
+        });
+
+        // create alert dialog
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        // show it
+        alertDialog.show();
     }
 }
