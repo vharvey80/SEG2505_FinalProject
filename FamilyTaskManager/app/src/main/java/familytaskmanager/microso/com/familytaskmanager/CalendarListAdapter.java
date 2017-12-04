34929f8f1b87081c051faf22fc5b5c4be82ede82
Jean-Gabriel Gaudreault <jean-gabriel.gaudreault@hotmail.com>
2017-12-04 18:51:47 -0500

    Minor UI changes on Calendar

--- FamilyTaskManager/app/src/main/java/familytaskmanager/microso/com/familytaskmanager/CalendarListAdapter.java
+++ FamilyTaskManager/app/src/main/java/familytaskmanager/microso/com/familytaskmanager/CalendarListAdapter.java
@@ -1,19 +1,11 @@
 package familytaskmanager.microso.com.familytaskmanager;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.res.Resources;
-import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
@@ -26,7 +18,7 @@ public class CalendarListAdapter extends ArrayAdapter<Task> {
     private List<Task> values;
 
     public CalendarListAdapter(Context context, List<Task> values) {
-        super(context, R.layout.task_list_item, values);
+        super(context, R.layout.taskcalendar_list_item, values);
         this.context = context;
         this.values = values;
     }
@@ -34,32 +26,20 @@ public class CalendarListAdapter extends ArrayAdapter<Task> {
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View rowView = inflater.inflate(R.layout.task_list_item, parent, false);
+        View rowView = inflater.inflate(R.layout.taskcalendar_list_item, parent, false);
 
         TextView taskName = (TextView) rowView.findViewById(R.id.taskListItemName);
-        TextView taskNote = (TextView) rowView.findViewById(R.id.tasksListItemNote);
-        ImageView taskIcon = (ImageView) rowView.findViewById(R.id.taskListItemIcon);
+        TextView taskUser = (TextView) rowView.findViewById(R.id.taskAssigned);
 
         Task task = values.get(position);
 
         taskName.setText(task.getTitle());
 
         //This is only place holder logic. Once I get UI done, make this fancier
-        if (task.hasNote()) {
-            taskNote.setText(task.getNote());
-        } else {
-            taskNote.setText("There is no note");
-        }
-
         if (task.hasUser()) {
-            //Getting profile pic
-            String resourceName = task.getUser().getProfilePicResourceName();
-            Resources resources = context.getResources();
-            int resourceId = resources.getIdentifier(resourceName, "drawable", "familytaskmanager.microso.com.familytaskmanager");
-            taskIcon.setImageResource(resourceId);
-            //end of getting picture
+            taskUser.setText("User: " + task.getUser().getFname());
         } else {
-            taskIcon.setImageResource(android.R.drawable.ic_menu_add);
+            taskUser.setText("No assigned user");
         }
 
         return rowView;
